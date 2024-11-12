715facaddc0faf8a1e78f2c1eaa8be7db829d290
ikicamilo <ikicamilo@gmail.com>
2024-11-11 22:58:56 -0500

    port is added

--- server.js
+++ server.js
@@ -37,6 +37,6 @@ sequelize
     console.error("Error syncing database:", error);
   });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
