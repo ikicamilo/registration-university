3586fcadaf3b699905c2d47f22e8170cd7251dba
ikicamilo <ikicamilo@gmail.com>
2024-11-12 00:04:10 -0500

    pool property is added

--- config/config.js
+++ config/config.js
@@ -14,5 +14,11 @@ module.exports = {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT || 3306,
     dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
   },
 };
