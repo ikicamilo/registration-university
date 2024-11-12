d586360eb101d71c3a86c020747663e51daf4453
ikicamilo <ikicamilo@gmail.com>
2024-11-11 22:46:16 -0500

    dev config is added

--- config/config.js
+++ config/config.js
@@ -1,4 +1,11 @@
 module.exports = {
+  development: {
+    username: "root",
+    password: "hola1234",
+    database: "universidad_espaniola",
+    host: "localhost",
+    dialect: "mysql",
+  },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
